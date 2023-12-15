820cd6a65787dc010329f88db33918bd7b50ece9
yewonkim301 <yeowne301@gmail.com>
2023-12-15 15:39:17 +0900

    fix: config

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 /* eslint-disable no-console */
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const path = require('path');
+const path = require("path");
+
+const db = require("./models/index");
+const dotenv = require("dotenv");
 
 const PORT = 8000;
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.use(express.static(path.join(__dirname, 'static')));
-const indexRouter = require('./routes/index');
-app.use('/', indexRouter);
+app.use(express.static(path.join(__dirname, "static")));
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
+// db.sequelize.sync({force: false}).then(() => {
 app.listen(PORT, () => {
   console.log(`${PORT}번 포트에서 실행중`);
 });
+// })
--- models/Index.js
+++ models/Index.js
@@ -1,31 +1,42 @@
-const Sequelize = require('sequelize');
-const config = require(__dirname + '/../config/config.json')['development'];
+const Sequelize = require("sequelize");
+const config = require(__dirname + "/../config/config.js")[env];
 
 const db = {};
 
 const sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-)
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-db.user = require('./user')(sequelize, Sequelize);
-db.public_community = require('./public-community')(sequelize, Sequelize);
-db.public_community_post = require('./public_community_post')(sequelize, Sequelize);
-db.public_community_like = require('./public_community_like')(sequelize, Sequelize);
-db.public_community_comment = require('./public_community_comment')(sequelize, Sequelize);
+db.user = require("./user")(sequelize, Sequelize);
+db.public_community = require("./public-community")(sequelize, Sequelize);
+db.public_community_post = require("./public_community_post")(
+  sequelize,
+  Sequelize
+);
+db.public_community_like = require("./public_community_like")(
+  sequelize,
+  Sequelize
+);
+db.public_community_comment = require("./public_community_comment")(
+  sequelize,
+  Sequelize
+);
 
-db.club_community = require('./club_community')(sequelize, Sequelize);
-db.club_community_post = require('./club_community_post')(sequelize, Sequelize);
-db.club_community_like = require('./club_community_like')(sequelize, Sequelize);
-db.club_community_comment = require('./club_community_comment')(sequelize, Sequelize);
+db.club_community = require("./club_community")(sequelize, Sequelize);
+db.club_community_post = require("./club_community_post")(sequelize, Sequelize);
+db.club_community_like = require("./club_community_like")(sequelize, Sequelize);
+db.club_community_comment = require("./club_community_comment")(
+  sequelize,
+  Sequelize
+);
 
-db.dm = require('./dm')(sequelize, Sequelize);
-db.schedule = require('./schedule')(sequelize, Sequelize);
-
+db.dm = require("./dm")(sequelize, Sequelize);
+db.schedule = require("./schedule")(sequelize, Sequelize);
 
 module.exports = db;
