a6a0637c91e1ecdf8b67ed4ab323abdad9ad806f
minsuje <minsu.jex@gmail.com>
2023-12-20 13:39:11 +0900

    feat: Support 라우터 연결

--- controller/Csupport.js
+++ controller/Csupport.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 exports.getSupport = async (req, res) => {
     try{
         const getSupport = await Support.findAll();
-        res.render("/supportMain", getSupport);
+        res.render("/support/supportMain", getSupport);
     }
     catch (err) {
         console.error(err);
@@ -32,13 +32,36 @@ exports.postSupport = async (req, res) => {
     }
 }
 
-// POST /supportMain 고객 문의 답글
+// PATCH /supportMain 고객 문의 답글
 exports.postSupportComment = async (req,res) => {
     try{
         const {comment} = req.body;
         const postSupportComment = await Support.update({
             comment: comment
         })
+        res.send(postSupportComment, {isSuccess:true});
+    }
+    catch (err) {
+        console.error(err);
+        res.send("Internal Server Error!");
+    }
+}
+
+// DELETE /supportMain 문의글 삭제
+exports.deleteSupport = async (req,res) => {
+    try{
+        const { userid, userid_num } = jwt.verify(req.cookies.jwt, process.env.JWT_SECRET);
+        const destroySupport = await Support.destroy({
+            where:
+            {
+                userid_num: userid_num
+            }
+        })
+        if (destroySupport) {
+            res.send({ isDeleted: true });
+        } else {
+            res.send({ isDeleted: false });
+        }
     }
     catch (err) {
         console.error(err);
