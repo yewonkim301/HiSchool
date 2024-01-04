2635fc248b53eb5eff64d6e070df614fc85a4d6a
yewonkim301 <yeowne301@gmail.com>
2024-01-04 20:22:21 +0900

    fix: socket namespace

--- controller/Csocket.js
+++ controller/Csocket.js
@@ -1,34 +1,34 @@
 const { User, Dm } = require("../models/Index");
 
-const roomList = [];
-exports.connection = (io, socket) => {
+const roomList2 = [];
+exports.connection = (namespace1, socket) => {
   console.log("접속 :", socket.id);
 
   //채팅방 목록 보내기
-  socket.emit("roomList", roomList);
+  socket.emit("roomList2", roomList2);
 
   //채팅방 만들기 생성
-  socket.on("create", async (roomName, userName, cb) => {
+  socket.on("create", async (roomName2, userName, cb) => {
     //join(방이름) 해당 방이름으로 없다면 생성. 존재하면 입장
     //socket.rooms에 socket.id값과 방이름 확인가능
-    socket.to(roomName).emit("notice", `${userName}님이 입장하셨습니다`);
-    socket.join(roomName);
+    socket.to(roomName2).emit("notice2", `${userName}님이 입장하셨습니다`);
+    socket.join(roomName2);
     //socket은 객체이며 원하는 값을 할당할 수 있음
-    socket.room = roomName;
+    socket.room = roomName2;
     socket.user = userName;
 
     //채팅방 목록 갱신
-    if (!roomList.includes(roomName)) {
-      roomList.push(roomName);
+    if (!roomList2.includes(roomName2)) {
+      roomList2.push(roomName2);
       //갱신된 목록은 전체가 봐야함 <=== 나와 상대방만 봐야해서 이제 바꿔야함
-      io.emit("roomList", roomList);
+      namespace1.emit("roomList2", roomList2);
     }
-    // const usersInRoom = getUsersInRoom(roomName);
-    // io.to(roomName).emit('userList', usersInRoom);
+    // const usersInRoom = getUsersInRoom(roomName2);
+    // io.to(roomName2).emit('userList', usersInRoom);
     cb();
   });
   //================ 위 까지 방만들기 =======================
-  socket.on("sendMessage", async (message) => {
+  socket.on("sendMessage2", async (message) => {
     console.log(">>>>>>>>>>>", message);
     console.log("Csocket Message From Nick >>>>", message.from_nick);
     const NewChatMessage = await Dm.create({
@@ -38,14 +38,12 @@ exports.connection = (io, socket) => {
       from_nick: message.from_nick,
     });
     console.log("NEW CHAT MESSAGE>>>>>>>>>>>>", NewChatMessage.dm_content);
-    io.to(socket.room).emit(
-      "newMessage",
-      NewChatMessage,
-      NewChatMessage.from_nick
-    );
+    namespace1
+      .to(socket.room)
+      .emit("newMessage2", NewChatMessage, NewChatMessage.from_nick);
   });
 
-  socket.on("disconnect", () => {
+  socket.on("disconnect2", () => {
     if (socket.room) {
       socket.leave(socket.room);
       console.log("연결 해제");
