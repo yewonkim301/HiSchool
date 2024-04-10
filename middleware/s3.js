801290d1965e7aef9041f369cb8d2cf26aa8f6c8
seejnn <seejinkim@gmail.com>
2024-04-10 15:51:36 +0900

    fix: s3 secret key

--- middleware/s3.js
+++ middleware/s3.js
@@ -11,8 +11,8 @@ const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 const s3 = new S3Client({
   credentials: {
-    accessKeyId: process.env.AWS_S3_KEY_ID,
-    secretAccessKey: process.env.AWS_S3_ACCESS_KEY,
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_S3_KEY_ID,
   },
   region: process.env.AWS_S3_REGION,
 });
